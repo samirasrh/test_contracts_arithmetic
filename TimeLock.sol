606776cdea77ca762604f1592cc64efed88e3faf
samirasrh <Samira.Briongos.extern@srh-hochschulen.de>
2024-11-13 01:39:20 +0100

    Create TimeLock.sol

---
+++ TimeLock.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: CC-BY-SA-4.0
+
+//Example from the Mastering Ethereum book
+// Version of Solidity compiler this program was written for
+pragma solidity ^0.6.4;
+
+contract TimeLock {
+
+    mapping(address => uint) public balances;
+    mapping(address => uint) public lockTime;
+
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+        lockTime[msg.sender] = now + 1 weeks;
+    }
+
+    function increaseLockTime(uint _secondsToIncrease) public {
+        lockTime[msg.sender] += _secondsToIncrease;
+    }
+
+    function withdraw() public {
+        require(balances[msg.sender] > 0);
+        require(now > lockTime[msg.sender]);
+        uint transferValue = balances[msg.sender];
+        balances[msg.sender] = 0;
+        msg.sender.transfer(transferValue);
+    }
+}
